1a216103fee88b503dc931c47d54f3f4c3e8708c
Jenkins <jenkins@loomx.io>
2018-07-31 12:27:57 +0000

    Update to version 334

--- loom.rb
+++ loom.rb
@@ -1,10 +1,10 @@
 class Loom < Formula
   desc "Loom"
   homepage "https://loomx.io/developers/"
-  url "https://private.delegatecall.com/loom/osx/build-333/loom"
-  sha256 "57551b014d88fa3030e9e42e528e6157776a22f09c5b32ec6a14c5861fd9d60d"
+  url "https://private.delegatecall.com/loom/osx/build-334/loom"
+  sha256 "d0060684a5ad56c570192d1526e8592d9f04e149e29a42d6eb8b9abf9da2a7e8"
   revision 1
-  version "333"
+  version "334"
 
   def install
     bin.install "loom"
