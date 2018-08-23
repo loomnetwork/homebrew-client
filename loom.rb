9f7a51b3f653c920d0e80b53880ed028c3a87e60
Jenkins <jenkins@loomx.io>
2018-08-23 08:24:23 +0000

    Update to version 401

--- loom.rb
+++ loom.rb
@@ -1,10 +1,10 @@
 class Loom < Formula
   desc "Loom"
   homepage "https://loomx.io/developers/"
-  url "https://private.delegatecall.com/loom/osx/build-400/loom"
-  sha256 "075c74b85fa414e5e1b42e628cb53a1c05d7ec62a29e5ef6e5f3f77d9f61ee3c"
+  url "https://private.delegatecall.com/loom/osx/build-401/loom"
+  sha256 "e0c8650f051622e36fce48810557ac29af7de3236e68533835adf5ae4b1e09ab"
   revision 1
-  version "400"
+  version "401"
 
   def install
     bin.install "loom"
