461117785c2230a61d57924eeb723dc20029aa0b
Jenkins <jenkins@loomx.io>
2018-09-29 14:25:09 +0000

    Update to version 454

--- loom.rb
+++ loom.rb
@@ -1,10 +1,10 @@
 class Loom < Formula
   desc "Loom"
   homepage "https://loomx.io/developers/"
-  url "https://private.delegatecall.com/loom/osx/build-453/loom"
-  sha256 "1b6bba5ffbb7a46bd43d34dcd870c2067ca4e999162530104933ac5152d2484d"
+  url "https://private.delegatecall.com/loom/osx/build-454/loom"
+  sha256 "861860e9def6e3f9968bf3d35b4bd6e6733ea97477e0fa6c611f15deae561b81"
   revision 1
-  version "453"
+  version "454"
 
   def install
     bin.install "loom"
