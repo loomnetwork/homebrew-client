2b532e21446b83e74acf07570df5060c007c1350
Jenkins <jenkins@loomx.io>
2018-11-26 11:29:07 +0000

    Update to version 24

--- loom.rb
+++ loom.rb
@@ -1,10 +1,10 @@
 class Loom < Formula
   desc "Loom"
   homepage "https://loomx.io/developers/"
-  url "https://private.delegatecall.com/loom/osx/build-23/loom"
+  url "https://private.delegatecall.com/loom/osx/build-24/loom"
   sha256 ""
   revision 1
-  version "23"
+  version "24"
 
   def install
     bin.install "loom"
