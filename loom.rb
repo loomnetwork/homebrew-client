0c582464f828f6c5e678a267f6066de5a3409765
Jenkins <jenkins@loomx.io>
2018-12-20 21:10:35 +0000

    Update to version 703

--- loom.rb
+++ loom.rb
@@ -1,10 +1,10 @@
 class Loom < Formula
   desc "Loom"
   homepage "https://loomx.io/developers/"
-  url "https://private.delegatecall.com/loom/osx/build-702/loom"
-  sha256 "daadeb632be5f602a94e5302e1d6a8f420e52b95871696660295d3e5285b6445"
+  url "https://private.delegatecall.com/loom/osx/build-703/loom"
+  sha256 "af6f72cc4bb645de8ae30c2e8cadbad54e3b144099873d5699c2b02ae6237811"
   revision 1
-  version "702"
+  version "703"
 
   def install
     bin.install "loom"
