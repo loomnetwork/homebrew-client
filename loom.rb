374301266249932505474d9736c8ae7fa6f62bc7
Jenkins <jenkins@loomx.io>
2019-09-18 08:41:38 +0000

    Update to version 1275

--- loom.rb
+++ loom.rb
@@ -1,10 +1,10 @@
 class Loom < Formula
   desc "Loom"
   homepage "https://loomx.io/developers/"
-  url "https://private.delegatecall.com/loom/osx/build-1274/loom"
-  sha256 "0e0847f52150048649758bb4a28b62003965d21fdf05afee537aef591ccf1a4d"
+  url "https://private.delegatecall.com/loom/osx/build-1275/loom"
+  sha256 "a05114c80eb812cec2d621715ea5888545c9eac849bec6a2884dfc0859b9072f"
   revision 1
-  version "1274"
+  version "1275"
 
   def install
     bin.install "loom"
